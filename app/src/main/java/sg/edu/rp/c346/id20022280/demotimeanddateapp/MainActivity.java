6b30cd81e0fca9f502c656e392cbb411e8f69480
20022280 <20022280@myrp.edu.sg>
2021-05-14 10:52:09 +0800

    v one

--- app/src/main/java/sg/edu/rp/c346/id20022280/demotimeanddateapp/MainActivity.java
+++ app/src/main/java/sg/edu/rp/c346/id20022280/demotimeanddateapp/MainActivity.java
@@ -3,6 +3,10 @@ package sg.edu.rp.c346.id20022280.demotimeanddateapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.TextView;
+import android.widget.TimePicker;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        DatePicker dp;
+        TimePicker tp;
+        Button btnDisplayDate;
+        Button btnDisplayTime;
+        TextView tvDisplay;
+
+        dp = findViewById(R.id.datePicker);
+        tp = findViewById(R.id.timePicker);
+        btnDisplayDate = findViewById(R.id.buttonDisplayDate);
+        btnDisplayTime = findViewById(R.id.buttonDisplayTime);
+        tvDisplay = findViewById(R.id.textViewDisplay);
+
+        
     }
 }
