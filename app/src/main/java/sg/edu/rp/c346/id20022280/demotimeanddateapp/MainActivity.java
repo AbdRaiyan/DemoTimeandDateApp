da54c6f6de031c54ccb8127f6334de3013711eb6
20022280 <20022280@myrp.edu.sg>
2021-05-14 13:44:22 +0800

    v two

--- app/src/main/java/sg/edu/rp/c346/id20022280/demotimeanddateapp/MainActivity.java
+++ app/src/main/java/sg/edu/rp/c346/id20022280/demotimeanddateapp/MainActivity.java
@@ -3,6 +3,7 @@ package sg.edu.rp.c346.id20022280.demotimeanddateapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.TextView;
@@ -20,13 +21,30 @@ public class MainActivity extends AppCompatActivity {
         Button btnDisplayDate;
         Button btnDisplayTime;
         TextView tvDisplay;
+        Button btnReset;
 
         dp = findViewById(R.id.datePicker);
         tp = findViewById(R.id.timePicker);
-        btnDisplayDate = findViewById(R.id.buttonDisplayDate);
+        btnDisplayDate = findViewById(R.id.buttonDisplayDate);s
         btnDisplayTime = findViewById(R.id.buttonDisplayTime);
         tvDisplay = findViewById(R.id.textViewDisplay);
+        btnReset = findViewById(R.id.buttonReset);
 
-        
+        btnDisplayTime.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String time = "Time is " + tp.getHour()+":"+tp.getMinute();
+                tvDisplay.setText(time);
+            }
+        });
+
+        btnReset.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                tp.setHour(0);
+                tp.setMinute(0);
+                dp.updateDate(2020,0,1);
+            }
+        });
     }
 }
